3dba8159b139aca264e36fb442ac066a494528a4
KellyCristinaAires <k.woys@hotmail.com>
2020-09-26 17:59:14 -0300

    atividade arrea triangulo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        double B;
+        double H;
+        double A;
+
+        Console.Write("Digite o valor da base ");
+        B = double.Parse(Console.ReadLine());
+         
+        Console.Write("Digite o valor da altura ");
+        H = double.Parse(Console.ReadLine());
+        Console.Write("\n");
+
+        Console.WriteLine ($"A área do triangulo é: {A = B * H /2} ");
+
+        Console.WriteLine ("Pressione para sair!");
+        Console.ReadKey();
+        }
+    }
+}
+
+            
+             
+
+
+
+
